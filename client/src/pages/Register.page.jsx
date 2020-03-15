7ba4d855d38f3659f67c10bc8c80d87796feeb46
Román Méndez <mendezbeck@gmail.com>
2020-03-15 08:29:13 +0100

    initial test pages

---
+++ client/src/pages/Register.page.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { user, register } from "../../lib/auth.api";
+import { RegisterUser } from "../components/RegisterUser";
+
+export const RegisterPage = withRouter(({ history }) => {
+  const setUser = user("set");
+
+  const handleRegister = async (type, obj) => {
+    const user = await register(type, obj);
+    setUser(user);
+    switch (type) {
+      case "user":
+        history.push("/register/dog");
+        break;
+      case "dog":
+        history.push("/register/human");
+        break;
+      case "human":
+        history.push("/");
+        break;
+      default:
+        history.push("/");
+    }
+  };
+  return (
+    <div>
+      <h2>Registrar</h2>
+      {/* <LoginSignupForm
+        handleSubmit={handleSubmit}
+      /> */}
+      <RegisterUser {...{ handleRegister }} />
+    </div>
+  );
+});
