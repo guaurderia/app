f72a43798c7d45ff0d8a48dceacebc220beacc06
Román Méndez <mendezbeck@gmail.com>
2020-03-15 08:03:57 +0100

    refactored register and login to use obj

--- client/lib/auth.api.js
+++ client/lib/auth.api.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
 import axios from "axios";
+import { userSchema } from "./schemas";
 
 export const UseContext = React.createContext();
 
@@ -17,61 +18,20 @@ export const user = action => {
 
 const api = axios.create({ baseURL: "http://localhost:3000" });
 
-export const registerUser = async (firstName, lastName, username, password, mainPhone, emergencyPhone, adress) => {
+export const register = async (type, obj) => {
   // Axios post a ruta /auth/signup en servidor
   // para crear un usuario en mongodb
   console.log(`Registrando usuario...`);
-  console.log(firstName, lastName, username, password, mainPhone, emergencyPhone, adress);
-  const res = await api.post("/register/user", {
-    firstName,
-    lastName,
-    username,
-    mainPhone,
-    emergencyPhone,
-    adress
-  });
+  console.log(obj);
+  const res = await api.post(`/register/${type}`, obj);
   console.log("Created User");
   console.log(res.data);
   return res.data;
 };
 
-export const registerDog = async (name, bread, age, user) => {
-  // Axios post a ruta /auth/signup en servidor
-  // para crear un usuario en mongodb
-  console.log(`Registrando usuario...`);
-  console.log(name, bread, age, user);
-  const res = await api.post("/register/dog", {
-    name,
-    bread,
-    age,
-    user
-  });
-  console.log("Created User");
-  console.log(res.data);
-  return res.data;
-};
-
-export const registerAdmin = async (firstName, lastName, roll, username, password) => {
-  console.log(`Registrando adminitrador...`);
-  console.log(firstName, lastName, username, password);
-  const res = await api.post("/register/admin", {
-    firstName,
-    lastName,
-    roll,
-    username,
-    password
-  });
-  console.log("Created User");
-  console.log(res.data);
-  return res.data;
-};
-
-export const loginUser = async (username, password) => {
+export const login = async (type, obj) => {
   console.log("Do Login");
-  const res = await api.post("/login/user", {
-    username,
-    password
-  });
+  const res = await api.post(`/login/${type}`, obj);
   console.log(res.data);
   return res.data;
 };
